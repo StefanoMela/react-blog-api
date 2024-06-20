a582db6fa82259b1348025a9dd5f0939bb7fe545
StefanoMela <stefano.mela25@gmail.com>
2024-06-20 16:35:53 +0200

    feat: fetch posts / client update / client delete

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,7 @@ export default function App() {
     setFormData((data) => ({ ...data, [name]: value }));
   };
 
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     if (editingIndex !== null) {
